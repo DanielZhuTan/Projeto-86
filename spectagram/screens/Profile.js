c091640aa839f5276b7cfc312322da253cf05e86
DanielZhuTan <93088122+DanielZhuTan@users.noreply.github.com>
2022-09-12 19:24:01 -0300

    Update Profile.js

--- spectagram/screens/Profile.js
+++ spectagram/screens/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Switch } from 'react-native-paper';
 
 export default class Profile extends Component {
     render() {
@@ -10,6 +11,13 @@ export default class Profile extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
+                <Switch
+                trackColor={{
+                  false: "#767577",
+                  true: this.state.light_theme ? "#eee" : "white"
+                }}
+                thumbColor={this.state.isEnabled ? "#ee8249" : "#f4f3f4"} 
+              />
                 <Text>Perfil</Text>
             </View>
         )
